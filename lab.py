f77d9b413b6f9ccae68d9de7fbe020cf28d0c4c9
ValterBeff <valterbeff.2001@gmail.com>
2024-09-19 08:42:28 -0300

    alterando o labirinto

--- lab.py
+++ lab.py
@@ -7,15 +7,29 @@ lb = [[0, 0, 1, 0, 0, 0],
       [0, 1, 0, 1, 1, 0],
       [0, 1, 0, 0, 0, 0]]
 
+test_lb = [[0, 0, 0, 0, 0, 1],
+      [0, 0, 0, 1, 1, 1],
+      [0, 0, 0, 1, 0, 1],
+      [0, 0, 1, 1, 0, 0],
+      [0, 0, 0, 1, 1, 0],
+      [0, 0, 0, 0, 1, 0],
+      [0, 1, 1, 1, 1, 0],
+      [0, 1, 0, 0, 0, 0]]
+
+
+
 def valor_labirinto(lista_desafio):
     encontrou_um = False
+    encontrou_zero = False
     for i in range(len(lista_desafio)):
-        if lista_desafio[i] == 1:
-            if not encontrou_um:
-                encontrou_um = True 
-            else:
-                lista_desafio[i] = 0 
+        if lista_desafio[i] == 1 and not encontrou_zero:
+            encontrou_um = True  
+        elif lista_desafio[i] == 0 and encontrou_um:
+            encontrou_zero = True  
+        elif lista_desafio[i] == 1 and encontrou_zero:
+            lista_desafio[i] = 0  
     return lista_desafio
 
 for lista in lb:
     print(valor_labirinto(lista))
+
